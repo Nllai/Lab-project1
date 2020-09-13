beee0d7cb50160d7d80284cc87c3304aecb4318a
Rushui Fang <rfang4@binghamton.edu>
2020-09-13 16:27:56 -0400

    Move images folder into src/Components

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
-
+import logo from "./images/logo.jpg";
 function Nav() {
     return (
         <nav className="navbar">
-            <Link to="/"></Link>
+            <Link to="/">
+                <img src={logo} alt="logo"></img>
+                <span>Healthland Clinic</span>
+            </Link>
             <div className="navbar-options">
                 <span>Home</span>
                 <span>Service</span>
@@ -13,6 +16,7 @@ function Nav() {
                 <span>Hiring</span>
                 <span>Contact</span>
             </div>
+            <button>Booking</button>
         </nav>
     );
 }
