8f5b9787518736755d83d18c26d39ebdd3aac85b
Rushui Fang <rfang4@binghamton.edu>
2020-09-18 14:27:19 -0400

    Add boostrap dropdown dependencies

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import $ from "jquery";
+import Popper from "popper.js";
 
 ReactDOM.render(
     <React.StrictMode>
